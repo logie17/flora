2e4cbf1e7bd78ad40d8c469e9a30606d00ff5b75
Logan Bell <loganbell@gmail.com>
2016-05-27 23:00:43 -0700

    Something compiles

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
 mod server;
-pub use server::FloraServer;
+
+pub struct FloraServer<'a> {
+    name: &'a str,
+}
+
+impl <'a> FloraServer<'a> {
+    pub fn new(name: &'a str) -> FloraServer<'a> {
+        FloraServer{name:name}
+    }
+}
+
 
