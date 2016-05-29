0a6894bbd3e1aab6a5e8456622bfd952e72fd61c
Logan Bell <loganbell@gmail.com>
2016-05-29 15:08:49 -0700

    Adding storage lookup

--- src/server.rs
+++ src/server.rs
@@ -1,15 +1,29 @@
 use super::authorize;
+use super::storage;
 
 pub struct FloraServer<'a> {
     name: &'a str,
+    storage: storage::Storage,
 }
 
 impl <'a> FloraServer<'a> {
     pub fn new(name: &'a str) -> FloraServer<'a> {
-        FloraServer{name:name}
+        FloraServer{name:name, storage: storage::Storage::new()}
     }
 
-    pub fn HandleAuthorizeRequest(redirect_uri: &'a str, state: &'a str, scope: &'a str) -> authorize::AuthorizeRequest<'a> {
-        return authorize::AuthorizeRequest::new(redirect_uri, state, scope);
+    pub fn HandleAuthorizeRequest(&self, client_id: &'a str, redirect_uri: &'a str, state: &'a str, scope: &'a str) -> authorize::AuthorizeRequest<'a> {
+        // TODO decode redirect_uri
+
+        let client = self.storage.GetClient(client_id);
+
+        if client.get_redirect_uri() == "" {
+            //todo better error handlling
+        }
+        
+        let ret = authorize::AuthorizeRequest::new(redirect_uri, state, scope);
+        
+
+        return ret;
     }
+
 }
