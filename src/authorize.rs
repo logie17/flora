1669544376da37df922c0e747ab21d5356ba41be
Logan Bell <loganbell@gmail.com>
2016-05-30 08:59:15 -0700

    Added request idea

--- src/authorize.rs
+++ src/authorize.rs
@@ -1,4 +1,5 @@
 pub struct AuthorizeRequest<'a> {
+    client_id: &'a str,
     redirect_uri: &'a str,
     state: &'a str,
     scope: &'a str,
@@ -6,8 +7,27 @@ pub struct AuthorizeRequest<'a> {
 }
 
 impl <'a> AuthorizeRequest<'a> {
-    pub fn new(redirect_uri: &'a str, state: &'a str, scope: &'a str) -> AuthorizeRequest<'a> {  
-        AuthorizeRequest{redirect_uri: redirect_uri, state: state, scope:scope, is_authorized: false}
+    pub fn new(client_id: &'a str, redirect_uri: &'a str, state: &'a str, scope: &'a str) -> AuthorizeRequest<'a> {  
+        AuthorizeRequest{
+            client_id: client_id,
+            redirect_uri: redirect_uri,
+            state: state,
+            scope:scope,
+            is_authorized: false,
+        }
+    }
+
+    pub fn client_id(&self) -> &str {
+        return self.client_id
+    }
+
+}
+
+pub struct AuthorizeResponse {}
+
+impl AuthorizeResponse {
+    pub fn new() -> AuthorizeResponse {  
+        AuthorizeResponse{}
     }
 
 }
