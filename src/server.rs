a409e1ecbaee129c673c4148a7eeb6397e651f0f
Logan Bell <loganbell@gmail.com>
2016-05-30 09:04:25 -0700

    modified to understand request/auth model

--- src/server.rs
+++ src/server.rs
@@ -10,20 +10,21 @@ impl <'a> FloraServer<'a> {
     pub fn new(name: &'a str) -> FloraServer<'a> {
         FloraServer{name:name, storage: storage::Storage::new()}
     }
-
-    pub fn HandleAuthorizeRequest(&self, client_id: &'a str, redirect_uri: &'a str, state: &'a str, scope: &'a str) -> authorize::AuthorizeRequest<'a> {
+//&self, client_id: &'a str, redirect_uri: &'a str, state: &'a str, scope: &'a str
+    pub fn HandleAuthorizeRequest(&self, response: &'a authorize::AuthorizeResponse, request: &'a authorize::AuthorizeRequest) -> bool {
         // TODO decode redirect_uri
 
-        let client = self.storage.GetClient(client_id);
-
-        if client.get_redirect_uri() == "" {
-            //todo better error handlling
+        if request.client_id() != "" {
+            let client = self.storage.GetClient(request.client_id());
+            if client.get_redirect_uri() == "" {
+                //todo better error handlling
+            }
         }
         
-        let ret = authorize::AuthorizeRequest::new(redirect_uri, state, scope);
+//        let ret = authorize::AuthorizeRequest::new(redirect_uri, state, scope);
         
 
-        return ret;
+        return true;
     }
 
 }
