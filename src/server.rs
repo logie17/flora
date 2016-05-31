ea62a73a89672aca7a4247b467fe6766284b0fde
Logan Bell <loganbell@gmail.com>
2016-05-30 23:16:49 -0700

    Setting redirct_uri

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,6 @@
 use super::authorize;
 use super::storage;
+use super::client;
 
 pub struct FloraServer<'a> {
     name: &'a str,
@@ -10,21 +11,21 @@ impl <'a> FloraServer<'a> {
     pub fn new(name: &'a str) -> FloraServer<'a> {
         FloraServer{name:name, storage: storage::Storage::new()}
     }
-//&self, client_id: &'a str, redirect_uri: &'a str, state: &'a str, scope: &'a str
-    pub fn HandleAuthorizeRequest(&self, response: &'a authorize::AuthorizeResponse, request: &'a authorize::AuthorizeRequest) -> bool {
-        // TODO decode redirect_uri
 
-        if request.client_id() != "" {
-            let client = self.storage.GetClient(request.client_id());
+    pub fn HandleAuthorizeRequest(&self, response: &'a mut authorize::AuthorizeResponse<'a>, request: &'a authorize::AuthorizeRequest) -> bool {
+        // TODO decode redirect_uri
+        let client_id: &'a str = request.client_id();
+        if !client_id.is_empty() {
+            let client = self.storage.GetClient(client_id);
             if client.get_redirect_uri() == "" {
                 //todo better error handlling
             }
+            let redirect_uri: &'a str = client.get_redirect_uri();
+            response.redirect_uri(redirect_uri);
+            return true;
         }
-        
-//        let ret = authorize::AuthorizeRequest::new(redirect_uri, state, scope);
-        
 
-        return true;
+        return false;
     }
 
 }
